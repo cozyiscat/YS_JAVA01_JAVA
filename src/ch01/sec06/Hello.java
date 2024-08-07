2532503ba8b425d9d731211d6adcdd8fea7eeab0
cozyiscat <yuiop7664@gmail.com>
2024-08-07 16:29:35 +0900

    8/7 chapter2 정수

--- src/ch01/sec06/Hello.java
+++ src/ch01/sec06/Hello.java
@@ -3,7 +3,10 @@ package ch01.sec06;
 public class Hello {
 
 	public static void main(String[] args) {
+		int x =2, y =3;
+		int result = x + y;
 		System.out.println("I can push for github");
+		
 
 	}
 
